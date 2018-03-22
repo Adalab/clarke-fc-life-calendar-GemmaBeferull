2bf6ccbdbae55a203991f5551cf37695dc289225
GemmaBeferull <gemmabecala@gmail.com>
2018-03-22 13:15:58 +0100

    npm install

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import Pokemon from './components/Pokemon';
+
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { };
+  }
+
+
+    render() {
+
+      return (
+
+<p>HOLA</p>
+
+      );
+    }
+  }
+
+  export default App;
