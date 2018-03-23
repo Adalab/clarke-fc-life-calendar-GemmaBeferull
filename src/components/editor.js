decbcaff713281d86acd9b82c19e2aedfc66bab0
GemmaBeferull <gemmabecala@gmail.com>
2018-03-23 19:43:57 +0100

    display text

--- src/components/editor.js
+++ src/components/editor.js
@@ -7,17 +7,36 @@ class Editor extends Component {
 constructor(props) {
   super(props);
   this.state = {
-    option: ''
+    option: '',
+    hidden: true
   };
-  this.handleChange = this.handleChange.bind(this);
 }
 
-handleChange(e) {
-  var selected = document.querySelector('input[name="options"]:checked').value;
+handleChange= (e) => {
   this.setState({
-    option: e.currentTarget.value
+    option: e.currentTarget.value,
+    hidden: true
+
   });
+
 }
+showMessages= (e) => {
+  this.setState({
+    option: e.currentTarget.value,
+    hidden: false
+  });
+
+}
+
+pushCancelButton = () => {
+  this.setState({
+    option: '',
+    hidden: true
+  });
+
+}
+
+
 
 render() {
   let date = new Date();
@@ -28,12 +47,10 @@ render() {
     if(day < 10) {
       day = '0' + day;
         }
-
-        if(month < 10) {
-          month = '0' + month;
+    if(month < 10) {
+      month = '0' + month;
         }
-
-        let today = day + '/' + month + '/' + year;
+    let today = day + '/' + month + '/' + year;
 
         const showMessage = this.state.hidden ? 'hidden' : '';
 
@@ -47,23 +64,22 @@ render() {
               </div>
               <h1>Selecciona una opción</h1>
               <form className= "flex">
-                <label for="o1"><input type="radio" value=":)" id="o1" name="options" onChange={this.handleChange}/> :)</label>
-                <label for="o2"><input type="radio" value=":(" id="o2" name="options" onChange={this.handleChange} /> :(</label>
+                <label for="o1"><input type="radio" value=":)" id="o1" name="options" className="label" onChange={this.showMessages}/> :)</label>
+                <label for="o2"><input type="radio" value=":(" id="o2" name="options" className="label" onChange={this.handleChange} /> :(</label>
 
               </form>
               <div>Marcado: <span>{this.state.option}</span></div>
+          <div className={`${showMessage}`}>
           <h1> Mensaje </h1>
-
-
           <form>
                <label for="message"></label>
                <textarea name="message" id="message" rows="4" cols="30" placeholder="¿Por que es un buen día?" onChange={this.props.onChangeUpdateText} value={this.props.textareaText} disabled={this.props.disabled}></textarea>
-               </form>
-
+          </form>
+</div>
 
           <Link to="/" style={{ textDecoration: 'none' }} className= "flex">
             <button type="button" name="button" className="saveButton">Guardar</button>
-            <button type="button" name="button" className="cancelButton">Cancelar</button>
+            <button type="button" name="button" className="cancelButton" onClick={this.pushCancelButton} >Cancelar</button>
           </Link>
           <Footer/>
         </div>
@@ -71,11 +87,5 @@ render() {
       );
     }
   }
-
-
-
-
-
-
 
   export default Editor;
